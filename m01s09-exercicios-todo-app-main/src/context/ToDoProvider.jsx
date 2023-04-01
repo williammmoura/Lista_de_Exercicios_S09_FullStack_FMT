b3ade028aa2683103f492bb70e1a0861127c5911
William Monte de Moura <williamonte@gmail.com>
2023-04-01 16:46:44 -0300

    Exercício 06

--- m01s09-exercicios-todo-app-main/src/context/ToDoProvider.jsx
+++ m01s09-exercicios-todo-app-main/src/context/ToDoProvider.jsx
@@ -3,10 +3,36 @@ import React, { createContext } from 'react';
 // Cria o contexto
 export const todoContext = createContext();
 
-const ToDoProvider = (props) =>{
-    return(
-        <todoContext.Provider value={undefined}>
-            {props.children}
-        </todoContext.Provider>
-    );
+const [toDos, setToDos] = useState([
+        { id: 1, text: "Estudar React", done: false },
+        { id: 2, text: "Praticar com um projeto", done: true },
+        { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
+    ]);
+
+    const addToDo = (text) => {
+    const newToDo = {
+        id: Date.now(),
+        text,
+        done: false,
+    };
+    setToDos([...toDos, newToDo]);
 };
+
+    const markTodo = (id) => {
+    const newToDos = toDos.map((todo) => {
+        if (todo.id === id) {
+        return { ...todo, done: !todo.done };
+        }
+    return todo;
+});
+    setToDos(newToDos);
+};
+
+const completedToDos = toDos.filter((todo) => todo.done);
+const pendingToDos = toDos.filter((todo) => !todo.done);
+
+return(
+    <todoContext.Provider value={addToDo, markTodo, completedToDos, pendingToDos}>
+        {children}
+    </todoContext.Provider>
+);
